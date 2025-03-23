a95dc88348d141a325c82a4825d968516b89c8dd
Ludwig Moreno <ludwig.moreno@gmail.com>
2025-03-23 18:39:33 -0400

    Se agregó index.html y DarkMode

--- dicom_app/urls.py
+++ dicom_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
-from .views import upload_dicom, serve_dicom_file, crop_dicom, clear_outputs, dicom_upload_view  # Add crop_dicom
+from .views import index, upload_dicom, serve_dicom_file, crop_dicom, clear_outputs, dicom_upload_view  # Add crop_dicom
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
     path('upload/', upload_dicom, name='upload_dicom'),
     path('dicom-upload/', dicom_upload_view, name='dicom-upload'),
     path('dicom/<str:filename>/', serve_dicom_file, name='serve_dicom_file'),
